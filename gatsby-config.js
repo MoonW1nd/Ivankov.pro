67cc5ac9e005ee362a6a3531a6f1876a25900961
Alexander Ivankov <alexanderivankoff@gmail.com>
2019-10-05 23:50:49 +0300

    build(npm): add ts-loader and update tscondfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Александр Иванков',
+    description: 'Разработчик интерфейсов',
+  },
   plugins: [
     `gatsby-plugin-sass`,
-    `gatsby-plugin-typescript`,
+    'gatsby-transformer-remark',
+    {
+      resolve: "gatsby-plugin-ts-loader",
+      options: {
+        tslint: true
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      }
+    }
   ],
 }
