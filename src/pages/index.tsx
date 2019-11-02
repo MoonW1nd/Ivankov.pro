4f639332bba6543fce6e133e3c8b6dc099263f93
Alexander Ivankov <moonw1nd@yandex-team.ru>
2019-11-02 19:50:56 +0300

    improvement: load fonts, no unused vars and props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
-import {Typography} from 'antd';
 import {graphql} from 'gatsby';
 import React from 'react';
 import AShape from '../components/a-shape';
 import Logo from '../components/logo';
-import {selectSiteMetadata} from '../selectors/siteMetadata';
 
 import styles from './styles.module.css';
 
 const gitHubLogo = require('../../static/github-seeklogo.com.svg');
-
-const {Text} = Typography;
 
 export const query = graphql`
     query {
@@ -22,20 +18,7 @@ export const query = graphql`
     }
 `;
 
-interface IMetaData {
-    site: {
-        siteMetadata: {
-            title: string;
-            description: string;
-        };
-    };
-}
-
-interface IMainPageProps {
-    data: IMetaData;
-}
-
-export const MainPage = ({data}: IMainPageProps) => (
+export const MainPage = () => (
     <div>
         <h1>
             <Logo />
