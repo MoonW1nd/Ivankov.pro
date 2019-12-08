c8d4e05b7dfb615013430e27067bce3d74a62204
Alexander Ivankov <alexanderivankoff@gmail.com>
2019-12-08 16:47:25 +0300

    fix(a-shape): new system mask, which work in Safari

--- src/components/a-shape/index.tsx
+++ src/components/a-shape/index.tsx
@@ -1,51 +1,18 @@
-import cn from 'classnames';
 import React from 'react';
 
-import styles from './styles.module.css';
-
-import images from './image';
+import * as types from './types';
 
 type AShapeType = 'develop' | 'blog' | 'design';
 
 interface IAShapeProps {
     type: AShapeType;
-    reverse?: boolean;
+    reflected?: boolean;
 }
 
-interface IClipPathProps {
-    id: string;
-    x: number | string;
-    y: number | string;
-    className: string;
-}
+const AShape = ({type}: IAShapeProps) => {
+    const Component = types[type];
 
-const getImageClassName = (type: AShapeType) =>
-    cn(styles.image, {
-        [styles.typeBlog]: type === 'blog',
-        [styles.typeDesign]: type === 'design',
-        [styles.typeDevelop]: type === 'develop',
-    });
-
-const ClipPath = ({id, x, y, className}: IClipPathProps) => (
-    <clipPath id={id}>
-        <text x={x} y={y} className={className}>
-            A
-        </text>
-    </clipPath>
-);
-
-const AShape = ({type}: IAShapeProps) => (
-    <div className={styles.root}>
-        <svg xmlns="http://www.w3.org/2000/svg">
-            <defs>
-                {type === 'design' && <ClipPath x="-500" y="-50" id="clip-design" className={styles.textReverse} />}
-                {type === 'develop' && <ClipPath x="85" y="600" id="clip-develop" className={styles.text} />}
-                {type === 'blog' && <ClipPath x="85" y="757" id="clip-blog" className={styles.text} />}
-            </defs>
-        </svg>
-
-        <img src={images[type]} className={getImageClassName(type)} />
-    </div>
-);
+    return <Component />;
+};
 
 export default AShape;
---
+++ src/components/a-shape/types/blog.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import AShape from '../baseComponent';
+import images from './image';
+
+const BlogAShape = () => <AShape imageUrl={images.blog} width={683} height={1002} x={-40} y={-250} />;
+
+export default BlogAShape;
