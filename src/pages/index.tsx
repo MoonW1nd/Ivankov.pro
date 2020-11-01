b780b07c23c1ffcff48dae1347bdacb3e01c59da
Alexander Ivankov <moonw1nd@yandex-team.ru>
2020-11-01 20:53:07 +0300

    feat(main): redesign main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import cn from 'classnames';
 import {graphql, Link} from 'gatsby';
 import React from 'react';
-import AShape from '../components/a-shape';
-import styles from './styles.module.css';
 
-import Header from '@components/header';
-import Icon from '@components/icon';
-import ExternalLink from '@components/link';
-import Logo from '@components/logo';
+import styles from './styles.module.css';
 
 export const query = graphql`
     query {
@@ -20,26 +14,49 @@ export const query = graphql`
     }
 `;
 
+// <ExternalLink href="https://github.com/MoonW1nd" className={styles.link} target={'_blank'}>
+//     <Icon type="gitHub" className={styles.gitHubLogo} />
+// </ExternalLink>
+//
 export const MainPage = () => (
     <div className={styles.root}>
-        <Header>
-            <h1>
-                <Logo showDescription={true} type="full" />
-            </h1>
-            <ExternalLink href="https://github.com/MoonW1nd" className={styles.link} target={'_blank'}>
-                <Icon type="gitHub" className={styles.gitHubLogo} />
-            </ExternalLink>
-        </Header>
-        <section className={cn(styles.navigation, styles.footerSpace)}>
-            <Link to="/develop/">
-                <AShape type="develop" />
-            </Link>
-            <Link to="/design/">
-                <AShape type="design" />
-            </Link>
-            <Link to="/blog/">
-                <AShape type="blog" />
-            </Link>
+        <section className={styles.contentWrapper}>
+            <div className={styles.content}>
+                <div>
+                    <h1 className={styles.name}>
+                        александр
+                        <br />
+                        иванков
+                    </h1>
+                </div>
+
+                <div>
+                    <p className={styles.about}>
+                        разработчик
+                        <br />
+                        дизайнер
+                        <br />
+                        vim евангелист
+                        <br />
+                    </p>
+
+                    <div className={styles.navigationWrapper}>
+                        <div className={styles.navigation}>
+                            <Link to="/develop/" className={styles.navigationItem}>
+                                разработка
+                            </Link>
+
+                            <Link to="/design/" className={styles.navigationItem}>
+                                дизайн
+                            </Link>
+
+                            <Link to="/blog/" className={styles.navigationItem}>
+                                блог
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     </div>
 );
